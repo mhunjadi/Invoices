9db8f05b58dd7ef001bc16df1628aa88f7733601
mhunjadi <marinhunjadi@gmail.com>
2024-03-25 11:38:03 +0100

    Refactoring and cleanup

--- Data/DbInitializer.cs
+++ Data/DbInitializer.cs
@@ -12,23 +12,25 @@ namespace Invoices.Data
                 return;   // DB has been seeded
             }
 
-            Random random = new Random();
-            var invoices = new Invoice[100];
+            Random random = new();
+            var invoices = new List<Invoice>();
             var customers = new List<Customer>();
             DateTime endDate = DateTime.Today;
             DateTime startDate = endDate.AddDays(-14);
             endDate = endDate.AddDays(4);
             TimeSpan timeSpan = endDate - startDate;
 
-            for (var i = 0; i < invoices.Length; i++)
+            for (var i = 0; i < 100; i++)
             {
-                TimeSpan newSpan = new TimeSpan(0, random.Next(0, (int)timeSpan.TotalMinutes), 0);
+                TimeSpan newSpan = new(0, random.Next(0, (int)timeSpan.TotalMinutes), 0);
                 DateTime newDate = startDate + newSpan;
-                Invoice invoice = new Invoice();
-                invoice.CustomerID = i % 10 == 0 ? random.Next(1000, 5000) : invoices[i - 1].CustomerID;
-                invoice.InvoiceDate = newDate;
-                invoice.TotalValue = (decimal)(random.NextSingle() * 1000);
-                invoices[i] = invoice;
+                Invoice invoice = new()
+                {
+                    CustomerID = i % 10 == 0 ? random.Next(1000, 5000) : invoices[i - 1].CustomerID,
+                    InvoiceDate = newDate,
+                    TotalValue = (decimal)(random.NextSingle() * 1000)
+                };
+                invoices.Add(invoice);
                 if(customers.FindIndex(x => x.ID == invoice.CustomerID) == -1)
                     customers.Add(new Customer() { ID = invoice.CustomerID, Name = "KUPAC " + invoice.CustomerID });
             }
